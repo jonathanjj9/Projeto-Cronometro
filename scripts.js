1d9cc8fe778666932e238a9555ecfcd265487ec5
jonathanjj9 <jonathan_silva9@outlook.com>
2024-06-07 12:49:25 -0300

    Cronometro digital

---
+++ scripts.js
@@ -0,0 +1,49 @@
+var second= 0
+var minute = 0
+var horas = 0
+var intervalo
+
+
+function doisDigitos(digito) {
+    if (digito < 10) {
+        return("0" + digito)
+    }else{
+        return(digito)
+    }
+}
+function start() {
+    intervalo = setInterval(contar, 1)
+}
+
+function stop() {
+    console.log("parou")
+    clearInterval(intervalo)
+
+}
+
+function zerar() {
+    minute= 0
+    second = 0
+    horas = 0
+    clearInterval(intervalo)
+    tempo.innerHTML = "00:00:00"
+}
+
+
+function contar() {
+    tempo.innerHTML = second++
+
+    if (second === 60) {
+        minute++
+        second = 0
+         
+         if (minute === 60) {
+            horas++
+            minute=0
+            
+         }
+
+
+    }
+    tempo.innerHTML = doisDigitos(horas) + ":" + doisDigitos(minute) + ":" + doisDigitos(second)
+}
